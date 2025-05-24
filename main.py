4eac89c85da108cbb228c16ca7b09ec204d803c5
Kaue-LS <kaue.ls0506@gmail.com>
2025-05-24 14:11:54 -0300

    first deploy: base, flask, template and css

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# @app.route("/api/mensagem", methods=["POST"])
+# def mensagem():
+#     data = request.json
+#     nome = data.get("nome")
+#     return jsonify({"resposta": f"Olá, {nome}! Esta é uma resposta do Flask."})
+
+if __name__ == "__main__":
+    app.run(debug=True)
