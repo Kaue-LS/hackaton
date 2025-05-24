beec0ecc37fc06906d5b29af5b7e1c4642fd77d5
Kaue-LS <kaue.ls0506@gmail.com>
2025-05-24 15:30:37 -0300

    feat: header desktop complete

---
+++ static/js/icons.js
@@ -0,0 +1,8 @@
+var camera = window.FontAwesome.icon({ prefix: 'fas', iconName: 'camer' })
+const svgIconLogo = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M12 2L2 12h3v8h6v-6h2v6h6v-8h3z"/>
+  </svg>
+`;
+
+document.getElementById("iconLogo").innerHTML = camera;
